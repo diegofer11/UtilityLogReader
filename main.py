3773aa42b360e2dcae5e1d8e71d43f0517ae11e4
Diego Ramirez Caballero <diego.fer659@gmail.com>
2020-07-26 13:06:38 -0500

    deleted production url

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from modules import logSearch as search
 from modules import logReader as reader
 
 reader.url_reader(
-    'https://srvvpcontrolcore.colsanitas.com/jboss/RIPSS/RipssPrestacionesEconomicasRS.log', 'PrestacionesEconomicas')
+    'https://remoteurl.com/log/filename_without_extension', 'desired_file_name')
 
 search.log_search(
-    'C:\Repositorios\python\logfile.log', 'keyword')
+    'C:\route_to_file\logfile.log', 'keyword')
